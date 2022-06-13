68bb43fac9088a7e8616549a80280a134bb41799
Luca Schneider <lschneidpro@hmail.com>
2022-06-13 06:42:40 +0200

    fix

--- ContosoDentistryChatBot/bot.js
+++ ContosoDentistryChatBot/bot.js
@@ -14,20 +14,24 @@ class DentaBot extends ActivityHandler {
         if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
         // create a QnAMaker connector
-        //this.QnAMaker = new QnAMaker()
-       
+        this.qnaMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
+
         // create a DentistScheduler connector
-      
+        this.dentistScheduler = new DentistScheduler(configuration.SchedulerConfiguration);
+
         // create a IntentRecognizer connector
+        this.intentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
 
 
         this.onMessage(async (context, next) => {
             // send user input to QnA Maker and collect the response in a variable
             // don't forget to use the 'await' keyword
-          
+            const qnaResults = await this.qnaMaker.getAnswers(context);
+
             // send user input to IntentRecognizer and collect the response in a variable
             // don't forget 'await'
-                     
+            const LuisResult = await this.intentRecognizer.executeLuisQuery(context);
+
             // determine which service to respond with based on the results from LUIS //
 
             // if(top intent is intentA and confidence greater than 50){
@@ -37,22 +41,59 @@ class DentaBot extends ActivityHandler {
             //  return;
             // }
             // else {...}
-             
+
+            if (LuisResult.luisResult.prediction.topIntent === "GetAvailability" &&
+                LuisResult.intents.GetAvailability.score > .5
+            ) {
+                // call scheduler api with location entity info
+                const availabilities = this.dentistScheduler.getAvailability();
+                console.log(availabilities)
+                await context.sendActivity(availabilities);
+                await next();
+                return;
+            }
+
+            if (LuisResult.luisResult.prediction.topIntent === "ScheduleAppointment" &&
+                LuisResult.intents.ScheduleAppointment.score > .5 &&
+                LuisResult.entities.$instance && 
+                LuisResult.entities.$instance.time && 
+                LuisResult.entities.$instance.time[0]
+            ) {
+                // call scheduler api with location entity info
+                const time = LuisResult.entities.$instance.time[0].text;
+                const responseText = this.dentistScheduler.scheduleAppointment(time);
+                console.log(responseText)
+                await context.sendActivity(responseText);
+                await next();
+                return;
+            }
+
+
+            if (qnaResults[0]) {
+                await context.sendActivity(`${qnaResults[0].answer}`);
+            }
+            else {
+                // If no answers were returned from QnA Maker, reply with help.
+                await context.sendActivity(`I'm not sure I can answer your question`
+                    + 'I can schedule an appointment'
+                    + 'Or you can ask me questions about the dentistry');
+            }
+
             await next();
-    });
+        });
 
         this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        //write a custom greeting
-        const welcomeText = '';
-        for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-            if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+            const membersAdded = context.activity.membersAdded;
+            //write a custom greeting
+            const welcomeText = 'Welcome to Dentistry Assistant. I can help you by answering your questions and scheduling appointments.';
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                }
             }
-        }
-        // by calling next() you ensure that the next BotHandler is run.
-        await next();
-    });
+            // by calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
     }
 }
 
